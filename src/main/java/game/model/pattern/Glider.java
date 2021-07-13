122a695439e337a9a3a40d4c4d0f4ebf1596a02d
Jörg Rückert <joerg.rueckert@icw.de>
2021-07-13 11:55:24 +0200

    * glider pattern initialization

---
+++ src/main/java/game/model/pattern/Glider.java
@@ -0,0 +1,34 @@
+package game.model.pattern;
+
+import static game.model.State.ALIVE;
+import static game.model.State.DEAD;
+import game.model.Cell;
+import java.util.List;
+
+public class Glider {
+
+    private final int startX;
+    private final int startY;
+
+    public Glider() {
+        this(0, 0);
+    }
+
+    public Glider(int startX, int startY) {
+        this.startX = startX;
+        this.startY = startY;
+    }
+
+    public List<Cell> pattern() {
+        return List.of(
+                new Cell(startX, startY, DEAD),
+                new Cell(startX, startY + 1, ALIVE),
+                new Cell(startX, startY + 2, DEAD),
+                new Cell(startX + 1, startY, DEAD),
+                new Cell(startX + 1, startY + 1, DEAD),
+                new Cell(startX + 1, startY + 2, ALIVE),
+                new Cell(startX + 2, startY, ALIVE),
+                new Cell(startX + 2, startY + 1, ALIVE),
+                new Cell(startX + 2, startY + 2, ALIVE));
+    }
+}
---
+++ src/test/java/game/model/pattern/GliderTest.java
@@ -0,0 +1,57 @@
+package game.model.pattern;
+
+import static game.model.State.ALIVE;
+import static game.model.State.DEAD;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import game.model.Cell;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class GliderTest {
+
+    @Test
+    void createGlider() {
+        Glider glider = new Glider();
+
+        List<Cell> cells = glider.pattern();
+
+        assertThat(cells).size().isEqualTo(9);
+    }
+
+    @Test
+    void createGliderOnSpecificPosition() {
+        Glider glider = new Glider(5, 6);
+
+        List<Cell> cells = glider.pattern();
+
+        assertThat(cells).extracting(Cell::getPositionX, Cell::getPositionY)
+                .containsExactly(
+                        tuple(5,6), tuple(5,7), tuple(5,8),
+                        tuple(6,6), tuple(6,7), tuple(6,8),
+                        tuple(7,6), tuple(7,7), tuple(7,8));
+    }
+
+    @Test
+    void createGliderOnDefaultPosition() {
+        Glider glider = new Glider();
+
+        List<Cell> cells = glider.pattern();
+
+        assertThat(cells).extracting(Cell::getPositionX, Cell::getPositionY)
+                .containsExactly(
+                        tuple(0,0), tuple(0,1), tuple(0,2),
+                        tuple(1,0), tuple(1,1), tuple(1,2),
+                        tuple(2,0), tuple(2,1), tuple(2,2));
+    }
+
+    @Test
+    void createGliderMatchesPattern() {
+        Glider glider = new Glider();
+
+        List<Cell> cells = glider.pattern();
+
+        assertThat(cells).extracting(Cell::getState)
+                .containsExactly(DEAD, ALIVE, DEAD, DEAD, DEAD, ALIVE, ALIVE, ALIVE, ALIVE);
+    }
+}
